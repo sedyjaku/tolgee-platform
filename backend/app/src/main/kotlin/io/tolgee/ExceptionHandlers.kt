0880fe3505e5dab9b689f294c7c5d66748f2761f
Jakub <jakub.sedy@cleevio.com>
2025-03-26 14:17:00 +0100

    fix: add stricter pattern for projectId urls and handle NumberFormatException if organizationId or projectId contain non-numeric values

--- backend/app/src/main/kotlin/io/tolgee/ExceptionHandlers.kt
+++ backend/app/src/main/kotlin/io/tolgee/ExceptionHandlers.kt
@@ -35,7 +35,8 @@ import org.springframework.web.multipart.MaxUploadSizeExceededException
 import org.springframework.web.multipart.support.MissingServletRequestPartException
 import org.springframework.web.servlet.resource.NoResourceFoundException
 import java.io.Serializable
-import java.util.*
+import java.util.Arrays
+import java.util.Collections
 import java.util.function.Consumer
 
 @RestControllerAdvice
@@ -282,4 +283,15 @@ class ExceptionHandlers : Logging {
     val headerXFF = request.getHeader("X-FORWARDED-FOR")
     logger.warn(message, request.method, request.requestURL, request.remoteAddr, headerXFF)
   }
+
+	@ExceptionHandler(InvalidPathException::class)
+	fun handleInvalidPathException(
+		exception: InvalidPathException
+	): ResponseEntity<ErrorResponseBody> {
+		val params = exception.message?.let { listOf(it) }
+		return ResponseEntity(
+			ErrorResponseBody(Message.INVALID_PATH.code, params),
+			HttpStatus.BAD_REQUEST,
+		)
+	}
 }
